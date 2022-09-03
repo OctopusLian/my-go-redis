e66748bc2a806a207f0f99775f986d5b001dd3c8
OctopusLian <zoctopus@qq.com>
2022-09-03 22:54:47 +0800

    优化 tcp server逻辑

--- tcp/server.go
+++ tcp/server.go
@@ -3,7 +3,7 @@
  * @Author: neozhang
  * @Date: 2022-09-03 21:27:08
  * @LastEditors: neozhang
- * @LastEditTime: 2022-09-03 22:31:33
+ * @LastEditTime: 2022-09-03 22:53:16
  */
 package tcp
 
@@ -12,6 +12,10 @@ import (
 	"mygoredis/interface/tcp"
 	"mygoredis/lib/logger"
 	"net"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 )
 
 // Config stores tcp server properties
@@ -21,6 +25,15 @@ type Config struct {
 
 func ListenAndServeWithSignal(cfg *Config, handler tcp.Handler) error {
 	closeChan := make(chan struct{})
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGHUP, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT)
+	go func() {
+		sig := <-sigChan
+		switch sig {
+		case syscall.SIGHUP, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT:
+			closeChan <- struct{}{}
+		}
+	}()
 	listener, err := net.Listen("tcp", cfg.Address)
 	if err != nil {
 		return err
@@ -30,17 +43,34 @@ func ListenAndServeWithSignal(cfg *Config, handler tcp.Handler) error {
 }
 
 func ListenAndServe(listener net.Listener, handler tcp.Handler, closeChan <-chan struct{}) error {
+	go func() {
+		<-closeChan
+		logger.Info("shutting down")
+		listener.Close()
+		handler.Close()
+	}()
+
+	defer func() {
+		listener.Close()
+		handler.Close()
+	}()
 	ctx := context.Background()
+	var waitDone sync.WaitGroup
 	for true {
 		conn, err := listener.Accept()
 		if err != nil {
 			return err
 		}
 		logger.Info("accepted link")
+		waitDone.Add(1) // 开始服务，+1
 		go func() {
+			defer func() {
+				waitDone.Done() //服务完，-1
+			}()
 			handler.Handle(ctx, conn)
 		}()
 	}
+	waitDone.Wait()
 
 	return nil
 }
